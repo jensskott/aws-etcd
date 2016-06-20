0a8e187961b0ffeab1439750f4d0a2ae6490ea96
Jens Skott <jens.skott@schibsted.se>
2016-06-20 21:32:28 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func main() {
+	// Create an EC2 service object in the "us-west-2" region
+	// Note that you can also configure your region globally by
+	// exporting the AWS_REGION environment variable
+	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("us-west-1")})
+	resp, err := svc.DescribeInstances(nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.Reservations)
+}
